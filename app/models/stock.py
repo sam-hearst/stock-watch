d39794eb14f287bcd29027f5459283363864c3d7
Sam Hearst <samhearst1013@gmail.com>
2021-04-16 01:08:03 -0400

    added to about section for stocks

--- app/models/stock.py
+++ app/models/stock.py
@@ -12,6 +12,10 @@ class Stock(db.Model):
     company_name = db.Column(db.String(100), nullable=False, unique=True)
     stock_ticker = db.Column(db.String(20), nullable=False, unique=True)
     company_info = db.Column(db.Text, nullable=False, unique=True)
+    num_employees = db.Column(db.Integer, nullable=False)
+    market_cap = db.Column(db.BigInteger, nullable=False)
+    pe_ratio = db.Column(db.Float, nullable=True)
+    dividend_yield = db.Column(db.Float, nullable=True)
 
     stock_details = db.relationship("Stock_Details", back_populates="stock")
 
@@ -26,5 +30,9 @@ class Stock(db.Model):
             "stocker_ticker": self.stock_ticker,
             "company_info": self.company_info,
             "quote": quote,
-            "stock_details": []
+            "stock_details": [],
+            "num_employees": self.num_employees,
+            "market_cap": self.market_cap,
+            "pe_ratio": self.pe_ratio,
+            "dividend_yield": self.dividend_yield
         }
